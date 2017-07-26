87e201a9e920e2772c384c3230732face21d82dc
Karlie Romas <karlie.romas@gmail.com>
2017-07-26 13:52:28 -0500

    read through ruby docs and discovered String methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+ "zom".insert(1,'o')
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ "the usual" + "suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+"z".sum(n=16)
+# => 122
+ (What is the significance of the number 122 in relation to the character z?) The significance is that the ASCII code for the letter Z is in fact the number 122. ASCII encodes 128 characters that include a-z all lowercase and A-Z all uppercase plus additional punctuation
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
