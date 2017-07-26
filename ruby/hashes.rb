819654446eb532a4dba99f3586818bfe98067453
Karlie Romas <karlie.romas@gmail.com>
2017-07-26 14:47:01 -0500

    add all client info to client_profile hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -29,4 +29,16 @@ puts "Decor Theme"
 puts "Budget"
   budget = gets.chomp.to_i
 
+# add all of the client's information to the client_profile hash
+client_profile[:client_name] = client_name
+client_profile[:client_age] = client_age
+client_profile[:client_address] = client_address
+client_profile[:client_email] = client_email
+client_profile[:client_number] = client_number
+client_profile[:child_count] = child_count
+client_profile[:decor_theme] = decor_theme
+client_profile[:budget] = budget
+
+p client_profile
+
 
