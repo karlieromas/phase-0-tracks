259bfee377b2c3d24c31d2ea470050e2b3a53b68
Karlie Romas <karlie.romas@gmail.com>
2017-07-26 18:45:38 -0500

    create nested data structure and driver code that demonstrates access

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,41 @@
+
+  room_number = {
+    karlies_desk: [
+    {
+      science_supplies: [
+      'folder',
+      'binder',
+      'compass',
+      'beaker'
+      ],
+      history_supplies: [
+      'history book',
+      'notebook',
+      'folder'
+      ]
+    }
+  ],
+   buddys_desk: [
+    {
+      math_supplies: [
+      'folder',
+      'binder',
+      'compass',
+      'ruler'
+      ],
+      art_supplies: [
+      'paper',
+      'colored pencils',
+      'pencils'
+      ]
+    }
+  ]
+}
+
+# room_number[:karlies_desk][0][:science_supplies]
+
+# room_number[:buddys_desk][0][:art_supplies][2]
+
+# room_number[:karlies_desk][0][:history_supplies][1]
+
+# room_number[:buddys_desk][0][:math_supplies]
