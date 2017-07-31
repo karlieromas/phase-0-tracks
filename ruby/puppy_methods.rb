78915cee10a8dabfa1fcf004208252eee1cf7ca6
Karlie Romas <karlie.romas@gmail.com>
2017-07-31 13:10:54 -0500

    create new instance methods for a new puppy class fido

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    number.times do
+      puts "woof"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(number)
+    p number*7
+  end
+
+  def eat(food)
+    puts "I ate the #{food}! Gimme more"
+  end
+
+
 end
+
+fido = Puppy.new
+fido.fetch("ball")
+
+fido.speak(5)
+fido.roll_over
+fido.dog_years(4)
+fido.eat("kibble")
 
