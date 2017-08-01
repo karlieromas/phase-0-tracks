d67a3f28e05acf4d08f838f3f1501fe63454ebc3
Karlie Romas <karlie.romas@gmail.com>
2017-08-01 14:14:54 -0500

    write three attribute changing methods to the Santa class. One method increases the santa's birthday by 1 year, the other gets mad at a reindeer and moves it to the last index of the array and gender is now writable. Write getter methods for age and ethnicity.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,6 @@
 class Santa
 
+
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -16,6 +17,29 @@ class Santa
     puts "That was a good #{cookie} cookie!"
   end
 
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender=(gender)
+    @gender = gender
+  end
+
+  def celebrate_birthday
+    @age = @age + 1
+    puts "I'm now #{@age} years old!"
+  end
+
+  def get_mad_at(reindeer_name)
+    reindeer_index = @reindeer_ranking.index(reindeer_name)
+    reindeer_found = @reindeer_ranking.delete_at(reindeer_index)
+    @reindeer_ranking << reindeer_found
+    puts @reindeer_ranking
+  end
 
 end
 
@@ -24,9 +48,16 @@ santas = []
 santas << santa_1 = Santa.new("female", "white")
 # santa_1.speak
 # santa_1.eat_milk_and_cookies("chocolate chip")
-
 santas << santa_2 = Santa.new("male", "hispanic")
-
 santas << santa_3 = Santa.new("bigender", "white")
 
-p santas
+# shows that the santa's birthday is going up by 1 each year
+santa_3.celebrate_birthday
+santa_3.celebrate_birthday
+
+# said reindeer gets moved to the last index of the array
+santa_3.get_mad_at("Cupid")
+
+# tests that we can retrive santa_1's age and ethnicity
+p santa_1.age
+p santa_1.ethnicity
