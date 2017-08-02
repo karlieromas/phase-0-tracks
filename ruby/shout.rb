e1d988f06453a7a34651e61b684fc4b058a8f137
Karlie Romas <karlie.romas@gmail.com>
2017-08-02 14:42:19 -0500

    make Shout module a mixin by adding two new methods and adding two new classes that include the Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,35 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + ":("
+  # def self.yell_angrily(words)
+  #   words + "!!!" + ":("
+  # end
+
+  # def self.yelling_happily(words)
+  #   words + " " + "wahoo!" + ":)"
+  # end
+
+  def yell(words)
+    puts "#{words} !!!"
   end
 
-  def self.yelling_happily(words)
-    words + " " + "wahoo!" + ":)"
+  def sing(words)
+    puts "You know you make me wanna #{words}!"
   end
+
 end
 
-p Shout.yell_angrily("crap")
-p Shout.yelling_happily("yesss")
+class Isley_Brothers
+  include Shout
+end
+
+class Lil_Jon
+  include Shout
+end
+
+# p Shout.yell_angrily("crap")
+# p Shout.yelling_happily("yesss")
+
+bro_1 = Isley_Brothers.new
+bro_1.sing("shout")
+
+lil_jon_1 = Lil_Jon.new
+lil_jon_1.yell("OK")
