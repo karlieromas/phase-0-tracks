4df88e1fb1dd4476264828f0a1ff441fd4007d5c
Karlie Romas <karlie.romas@gmail.com>
2017-08-08 10:45:45 -0500

    write a constructor function to make a new car

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,25 @@ for (var i = 0; i < horse_names.length; i++) {
 };
 
 console.log(horses);
+
+function Car(model, year, honk) {
+  console.log("Our new car:", this);
+  this.model = model;
+  this.year = year;
+  this.honk = function(){
+    console.log("honk!");
+};
+    console.log("CAR INITIALIZATION COMPLETE");
+  };
+
+console.log("Let's build a car!")
+var shelby_Cobra = new Car ("Shelby Cobra", 1969);
+console.log(shelby_Cobra.model);
+console.log(shelby_Cobra.year);
+shelby_Cobra.honk();
+
+var Corvette = new Car ("Corvette", 1953);
+console.log(Corvette.model);
+console.log(Corvette.year);
+Corvette.honk();
+
