8526f223e515af728d83a04099cd1dac1f2b629c
Karlie Romas <karlie.romas@gmail.com>
2017-08-08 13:36:12 -0500

    create function that checks key value pairs for matches and will either return true or false

--- js/algos.js
+++ js/algos.js
@@ -23,3 +23,25 @@ console.log(findLongestPhrase(tvArray));
 console.log(findLongestPhrase(looneyTunesArray));
 console.log(findLongestPhrase(animalsArray));
 
+
+//declare two different objects and compare the name and age of both of the objects
+//return true only if one of the keys or values match up
+//else return false
+
+var pup = {name: "Tucker", age: 3};
+var child = {name: "Avienne", age: 9};
+var dog = {name: "Lole", age: 3};
+
+function checkKeyValuePairs(object_1, object_2) {
+  if (object_1.name == object_2.name || object_1.age == object_2.age) {
+  return(true);
+}
+else {
+  return(false);
+}
+}
+
+console.log(checkKeyValuePairs(pup, child));
+console.log(checkKeyValuePairs(pup, dog));
+console.log(checkKeyValuePairs(child, dog));
+
