53f00795d54d0fb9f9af101b424326f41d690fe6
Karlie Romas <karlie.romas@gmail.com>
2017-08-08 10:31:42 -0500

    write a for loop to assign colors to horses in a new horses hash

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,4 +4,9 @@ var horse_names = ["Ed", "Bob", "Susie", "Jane"];
 horse_names.push("Lance");
 horse_colors.push("pink");
 
+var horses = {};
+for (var i = 0; i < horse_names.length; i++) {
+  horses[horse_names[i]] = horse_colors[i];
+};
 
+console.log(horses);
